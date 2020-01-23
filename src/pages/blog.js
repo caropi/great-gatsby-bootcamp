f284b05e252d0ae32f26cc24f29deaa87dca300e
caropi <pisanocaroline@gmail.com>
2020-01-23 10:32:22 -0500

    adding graphql rendering of blog post titles

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import {useStaticQuery, graphql} from 'gatsby'
 
 import Layout from "./components/layout"
 
 
 const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+      query {
+        allMarkdownRemark {
+          edges {
+            node {
+              frontmatter {
+                title
+                date
+            }
+          }
+        }
+        }
+      }
+    `)
     return (
         <Layout>
             <h1>Blog</h1>
-            <p>Posts will show up here later on</p>
+                    <ol className="blogPost" >
+            {data.allMarkdownRemark.edges.map((d, i) => {
+                return (
+                        <li key={i}>
+                            <h2>{d.node.frontmatter.title}</h2>
+                            <p>{d.node.frontmatter.date}</p>
+                        </li>
+
+                )
+            })}
+                    </ol>
         </Layout>
     )
 }

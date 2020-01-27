123c15cb97f81d27738cef3da3c10c99095db8e0
caropi <pisanocaroline@gmail.com>
2020-01-27 10:37:06 -0500

    tweaking templates and gatsby-node

---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../pages/components/layout'
+
+export const Blog = () => {
+    return (
+        <Layout>
+            This is a blog template
+        </Layout>
+    )
+}
